3f97f5f78ae09e9a45c1c3f54951ef3e32f15de9
kalumbamon <hsbabdulkadil22@src.pshs.edu.ph>
2025-02-13 08:41:01 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function ohno(){
+    let pInitial = parseFloat(prompt("sigma ano initial"));
+    let r = parseFloat(prompt("RATEEEEEEEEE"));
+    let t = parseFloat(prompt("TIME MOOO"));
+
+    let pFinal = Math.round(pInitial * Math.pow(Math.E, r*t));
+    let regionOfTheMonster = prompt("region mo");
+    let nameOfTheMonster = prompt("NAMEEEEE");
+    let halaaaa
+}
